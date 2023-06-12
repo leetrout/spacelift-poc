3e3d77e51e4ef15030e123487458c4eb40b1f431
Lee Trout <ltrout@crunchtime.com>
2023-06-12 12:40:11 -0400

    Add workerpool

---
+++ workerpool/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.48.0"
+    }
+  }
+}
+
+# Copied from https://github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2
+module "opsx_workerpool" {
+  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v1.6.0"
+
+  configuration = <<-EOT
+    export SPACELIFT_TOKEN="${var.worker_pool_config}"
+    export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
+  EOT
+
+  min_size          = 0
+  max_size          = 1
+  worker_pool_id    = var.worker_pool_id
+  security_groups   = var.worker_pool_security_groups
+  vpc_subnets       = var.worker_pool_subnets
+}
